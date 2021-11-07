a5ea12086c629f4683ef89a7704584a3f487055c
Iaroslav Mokroguz <maustrauk@gmail.com>
2021-11-07 11:41:41 -0500

    Adding User and Email Logger

--- pizza-palace/src/main/java/com/revature/services/UserService.java
+++ pizza-palace/src/main/java/com/revature/services/UserService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.revature.log.UserLog;
 import com.revature.models.User;
 import com.revature.repositories.UserRepository;
 import com.revature.utils.EmailSender;
@@ -13,28 +14,34 @@ import com.revature.utils.PasswordEncrypter;
 @Service
 public class UserService {
 	private UserRepository uRepo;
-	private EmailSender email = new EmailSender();
-	private PasswordEncrypter encr = new PasswordEncrypter();
-	private String salt = "itMbrvNjVoId0qpezuvwvWIcybb0rI";
+	private UserLog userLog;
+	private EmailSender email;
+	private PasswordEncrypter encr;
+	private final String salt = "itMbrvNjVoId0qpezuvwvWIcybb0rI";
 
 	public UserService() {
 		// TODO Auto-generated constructor stub
 	}
 
 	@Autowired
-	public UserService(UserRepository uRepo) {
+	public UserService(UserRepository uRepo, UserLog userLog, EmailSender email, PasswordEncrypter encr) {
 		super();
 		this.uRepo = uRepo;
+		this.userLog = userLog;
+		this.email = email;
+		this.encr = encr;
 	}
 
 	public List<User> getAllUser() {
+		userLog.infoLogger("UserService: Getting all users from DB.");
 		return uRepo.findAll();
 	}
 	
 	public User insertUser(User user) {
+		userLog.infoLogger("UserService: insertUser() invoked.");
 		User verifyUser = uRepo.findByUserName(user.getUserName());
 		if (verifyUser != null) {
-			System.out.println("User already exist");
+			userLog.errorLogger("UserService: User already exist");
 			return null;
 		}
 		
@@ -49,14 +56,16 @@ public class UserService {
 		
 		email.sendHTMLEmail(user.getUserEmail(), "Creating account at Pizza Palace", email.HTMLWraper(htmlBody));
 		
+		userLog.infoLogger("UserService: " + user.toString() + " added to DB.");
 		return uRepo.findByUserName(user.getUserName());
 	}
 	
 	public User updateUser(User user) {
+		userLog.infoLogger("UserService: updateUser() invoked.");
 		User updatedUser = uRepo.findByUserName(user.getUserName());
 		
 		if(updatedUser == null) {
-			System.out.println("Such user doesn't exist");
+			userLog.errorLogger("UserService: Such user doesn't exist");
 			return null;
 		}
 	
@@ -79,33 +88,37 @@ public class UserService {
 		
 		email.sendHTMLEmail(user.getUserEmail(), "Updating account at Pizza Palace", email.HTMLWraper(htmlBody));
 		
+		userLog.infoLogger("UserService: " + updatedUser.toString() + " updated.");
 		return uRepo.findByUserName(updatedUser.getUserName());
 	}
 	
 	public User getUserByName(String name) {
+		userLog.infoLogger("UserService: getUserByName() invoked.");
 		return uRepo.findByUserName(name);
 	}
 	
 	public User getUserById(int id) {
+		userLog.infoLogger("UserService: getUserById() invoked.");
 		return uRepo.findByUserId(id);
 	}
 	
 	public void deleteUser(User user) {
+		userLog.infoLogger("UserService: deleteUser() invoked.");
 		uRepo.delete(user);
+		userLog.infoLogger("UserService: " + user.toString() + " deleted from DB.");
 	}
 	
 	public User verifyPassword(User user) {
+		userLog.infoLogger("UserService: verifyPassword() invoked.");
 		User verifyUser = uRepo.findByUserName(user.getUserName());
 		
-		System.out.println("Verify User: " + verifyUser);
-		
 		if (verifyUser == null) {
-			System.out.println("There no such user");
+			userLog.errorLogger("UserService: There is no such user");
 			return null;
 		}
 		
 		if(!getEncrypteString(user.getUserPassword()).equals(verifyUser.getUserPassword())) {
-			System.out.println("Passwords didn't matched");
+			userLog.errorLogger("UserService: Passwords didn't matched");
 			return null;
 		}
 		
@@ -113,6 +126,7 @@ public class UserService {
 	}
 	
 	public String getEncrypteString(String str) {
+		userLog.infoLogger("UserService: getEncrypteString() invoked.");
 		return this.encr.generateSecurePassword(str, salt);
 	}
 	
